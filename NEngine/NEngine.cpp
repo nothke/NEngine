c0dc4778542aa8e185b5aa11b0d8d05b1635b589
Ivan Notaros <nothke@gmail.com>
2020-03-13 17:11:25 +0100

    Window

--- NEngine/NEngine.cpp
+++ NEngine/NEngine.cpp
@@ -7,4 +7,31 @@
 int main()
 {
 	LOG("First line!");
+
+	GLFWwindow* window;
+
+	if (!glfwInit())
+		return -1;
+
+	window = glfwCreateWindow(800, 600, "NEngine", NULL, NULL);
+
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(window);
+
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
 }
