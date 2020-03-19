54dd2aee4d770097d08e51729624247bf2d4103a
Ivan Notaros <nothke@gmail.com>
2020-03-19 22:26:42 +0100

    Rendering is now much faster with optimized vertices!

--- NEngine/Mesh.cpp
+++ NEngine/Mesh.cpp
@@ -2,7 +2,7 @@
 #include "GLAssert.h"
 #define GLEW_STATIC
 #include "GL/glew.h"
-#include "meshoptimizer.h"
+#include "meshoptimizer/meshoptimizer.h"
 
 void Mesh::Bind()
 {
@@ -32,14 +32,33 @@ void Mesh::Bind()
 
 void Mesh::Init(std::vector<Vertex>& vertVector, std::vector<unsigned int>& indicesVector)
 {
+	// 1. Indexing
+	size_t ic = indicesVector.size();
+	std::vector<unsigned int> remap(ic);
+	size_t vc = meshopt_generateVertexRemap(&remap[0], &indicesVector[0], ic, &vertVector[0], ic, sizeof(Vertex));
+
+	std::vector<unsigned int> resultIndices;
+	std::vector<Vertex> resultVertices;
+
+	resultIndices.resize(ic);
+	meshopt_remapIndexBuffer(&resultIndices[0], &indicesVector[0], ic, &remap[0]);
+
+	resultVertices.resize(vc);
+	meshopt_remapVertexBuffer(&resultVertices[0], &vertVector[0], ic, sizeof(Vertex), &remap[0]);
+
+	// 2. Vertex cache optimization
+	meshopt_optimizeVertexCache(&resultIndices[0], &resultIndices[0], ic, vc);
+
+	vertVector = resultVertices;
+	indicesVector = resultIndices;
+
+	std::cout << ic << " " << indicesVector.size() << std::endl;
+
 	vertices = &vertVector[0];
 	indices = &indicesVector[0];
 
 	vertexCount = vertVector.size();
 	indexCount = indicesVector.size();
-
-	std::vector<unsigned int> remap(indexCount);
-	meshopt_generateVertexRemap(&remap[0], NULL, indexCount, &vertices[0], indexCount, sizeof(Vertex));
 }
 
 Mesh::Mesh()
