93afdad38e185db30cb9fe083d4b88e9c69bbcfc
Ivan Notaros <nothke@gmail.com>
2020-03-19 15:29:14 +0100

    Shader strip

--- NEngine/res/vertexcolor.glsl
+++ NEngine/res/vertexcolor.glsl
@@ -4,8 +4,8 @@
 in vec4 position; // layout(location = 0) // not needed, apparently
 in vec4 color; // layout(location = 3)
 
-uniform vec4 _InputColor1;
-uniform vec4 _InputColor2;
+//uniform vec4 _InputColor1;
+//uniform vec4 _InputColor2;
 uniform mat4 _VP;
 uniform mat4 _M;
 uniform float _Mult;
@@ -15,16 +15,18 @@ out vec4 out_color;
 
 void main(){
 	mat4 mvp = _VP * _M;
+	//mat4 mvp = _M;
 	gl_Position = mvp * position;
 	//gl_FrontColor = color;
 	//out_color = color;
 	
-	float grad = (2 + position.y)*0.2;
-	grad += length(position) * _Range;
-	grad = clamp(grad * _Mult, 0, 1);
-    //vec3 vc = mix(vec3(0.7,0.3,1), vec3(0,1,1), grad);
-	vec3 vc = mix(_InputColor1.rgb, _InputColor2.rgb, grad);
-	out_color = color * vec4(vc, 1);
+	//float grad = (2 + position.y)*0.2;
+	//grad += length(position) * _Range;
+	//grad = clamp(grad * _Mult, 0, 1);
+	//vec3 vc = mix(_InputColor1.rgb, _InputColor2.rgb, grad);
+	//out_color = color * vec4(vc, 1);
+
+	out_color = color;
 }
 
 #shader fragment
