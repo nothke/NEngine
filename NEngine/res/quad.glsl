b65a8f0df9de62be7dc27fc142ce11db6b7dba30
Ivan Notaros <nothke@gmail.com>
2020-04-13 18:01:02 +0200

    Working frame buffer with scene view!

--- NEngine/res/quad.glsl
+++ NEngine/res/quad.glsl
@@ -17,10 +17,9 @@ out vec4 FragColor;
   
 in vec2 TexCoords;
 
-//uniform sampler2D screenTexture;
+uniform sampler2D screenTexture;
 
 void main()
 { 
-    //FragColor = texture(screenTexture, TexCoords);
-    FragColor = vec4(1,0,0,1);
+    FragColor = texture(screenTexture, TexCoords);
 }
